b97ae79a486ceca371213fad44459ef4332000a7
ToySteel <equipesesi.toyssteel@gmail.com>
2024-11-17 18:55:27 -0300

    Cu

--- toystubarao-tututututut/scripts/heart.gd
+++ toystubarao-tututututut/scripts/heart.gd
@@ -1,17 +1,25 @@
 extends Area2D
+#Onready var's
 @onready var animation = $animation
-
 @onready var colision = $colision
-@export var magma_type = ""
+@onready var dangers = $"../../dangers"
+#var's
 var state = "Normal"
-var tentativa = name
+
+#verificar a animaçao atual a cada processo
 func _process(delta):
 	_state()
-func _on_body_entered(body):
-	state = "ice"
-	get_parent().get_parent().get
-	print(get_parent().get_parent().name)
+#verificador de animaçao atual
 func _state():
 	if animation.name != state:
 		animation.play(state)
+#verificador de colisao
+func _on_body_entered(body):
+	#modificador de estado da animaçao
+	state = "ice"
+	#dangers.get_child(tentativa)
+	#envia sinal para desativar a area perigosa adjacnte
+	print(get_parent().get_node("L" + name))
+	get_parent().get_node("L" + name).desativar()
+
 	
--- toystubarao-tututututut/scripts/magma_jorradas.gd
+++ toystubarao-tututututut/scripts/magma_jorradas.gd
@@ -1,10 +1,17 @@
 extends Area2D
+
+@onready var timer_ativo = $"Timer ativo"
+@onready var timer_desativo = $"Timer desativo"
+
 @onready var anim = $aniamtion
 @onready var colision = $colision
 
-@export var magma_type = ""
+@export var Jorravel = false
 
 var state = "Jorrando"
+func _ready():
+	timer_desativo.start()
+
 func _process(delta):
 	_state()
 func _on_body_entered(body):
@@ -14,6 +21,18 @@ func _state():
 	if anim.name != state:
 		anim.play(state)
  
+func desativar():
+	colision.disabled = true
+	state = "Desligado"
+	Jorravel = false
 
-#se receber sinal DESLIGANDO desligar
-#se a variavel export magma type for Ocasional Timer.start
+func _on_timer_ativo_timeout():
+	if Jorravel:
+		state = "Jorrando"
+		colision.disabled = false
+		timer_desativo.start()
+func _on_timer_desativo_timeout():
+	if Jorravel:
+		state = "Ligado"
+		colision.disabled = true
+		timer_ativo.start()
